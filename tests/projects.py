6da1a617b0c377e85e274aac744e83fd0953c838
Peter Hodge <peter.hodge84@gmail.com>
2024-05-03 13:14:40 +1000

    Redo other fixtures

--- tests/projects.py
+++ tests/projects.py
@@ -3,7 +3,7 @@ import tempfile
 from dataclasses import dataclass
 from pathlib import Path
 from textwrap import dedent
-from typing import Iterator
+from typing import Callable, Iterator
 
 import pytest
 
@@ -29,6 +29,9 @@ class GitRepo:
     path: Path
     file1: Path
     file2: Path
+    pyfile: Path
+
+    _repo_id: str | None = None
 
     def run_git(self, args, **kwargs):
         cmd = [
@@ -44,11 +47,47 @@ class GitRepo:
 
         return subprocess.run(cmd, cwd=self.path, check=check, stdout=stdout, **kwargs)
 
+    def get_repo_id(self):
+        # Note: this needs to replicate the logic from
+        # autoload#vimprojectconfig#_utils <SID>getGitProjectId()
+        if self._repo_id is None:
+            cmd = ['git', '-C', self.path, 'rev-list', '--max-parents=0', 'HEAD']
+            sha = subprocess.check_output(cmd, encoding='utf-8').strip()
+            assert len(sha)
+            self._repo_id = sha
+        return self._repo_id
+
+
+def _git_repo_maker(
+    repo_path: Path,
+    repo_files: dict[str, str],
+    *,
+    shortcuts: dict[str, str],
+) -> GitRepo:
+    repo_path.mkdir()
+
+    # create some files
+    file_kwargs: dict[str, Path] = {}
+    for basename, contents in repo_files.items():
+        filepath = (repo_path / basename)
+        filepath.write_text(dedent(contents))
+        file_kwargs[f'file{len(file_kwargs) + 1}'] = filepath
+
+    for argname, filename in shortcuts.items():
+        file_kwargs[argname] = repo_path / filename
+
+    repo = GitRepo(path=repo_path, **file_kwargs)  # type: ignore
+
+    repo.run_git(['init'])
+    repo.run_git(['add'] + list(repo_files.keys()))
+    repo.run_git(['commit', '-m', 'initial commit'])
+
+    return repo
+
 
 @pytest.fixture
 def git_repo_a(tmpdir):
     repo_path = tmpdir / 'git-repo-a'
-    repo_path.mkdir()
 
     repo_files = {
         "README.md": (
@@ -64,19 +103,69 @@ def git_repo_a(tmpdir):
             '''
         )
     }
+    shortcuts = {'pyfile': 'test.py'}
 
-    # create some files
-    for basename, contents in repo_files.items():
-        (repo_path / basename).write_text(dedent(contents))
+    return _git_repo_maker(repo_path, repo_files, shortcuts=shortcuts)
 
-    repo = GitRepo(
-        path=repo_path,
-        file1=repo_path / 'README.md',
-        file2=repo_path / 'test.py',
-    )
 
-    repo.run_git(['init'])
-    repo.run_git(['add'] + list(repo_files.keys()))
-    repo.run_git(['commit', '-m', 'initial commit'])
+@pytest.fixture
+def git_repo_b(tmpdir):
+    repo_path = tmpdir / 'git-repo-b'
 
-    return repo
+    repo_files = {
+        "README.md": (
+            '''
+            # Welcome to git repo B.
+            '''
+        ),
+        "system.py": (
+            '''
+            import subprocess, sys
+
+            subprocess.run(sys.argv[1], check=True)
+            '''
+        )
+    }
+    shortcuts = {'pyfile': 'system.py'}
+
+    return _git_repo_maker(repo_path, repo_files, shortcuts=shortcuts)
+
+
+@pytest.fixture
+def git_repo_c(tmpdir):
+    repo_path = tmpdir / 'git-repo-c'
+
+    repo_files = {
+        "README.md": (
+            '''
+            # Welcome to git repo C.
+            '''
+        ),
+        "hello.py": (
+            '''
+            import sys
+
+            sys.stdout.write("Hello, world!\n")
+            '''
+        )
+    }
+    shortcuts = {'pyfile': 'hello.py'}
+
+    return _git_repo_maker(repo_path, repo_files, shortcuts=shortcuts)
+
+
+@pytest.fixture
+def repo_config_factory(personalconfigsdir: Path) -> Callable[..., None]:
+    def _factory(repo: GitRepo, slug: str, desc: str, contents: str) -> None:
+        projectid = repo.get_repo_id()
+        cfgpath = personalconfigsdir / slug
+        cfgpath.mkdir(exist_ok=False, parents=True)
+        projectvim = cfgpath / 'project.vim'
+        with projectvim.open('w') as f:
+            f.write(f'" Project: {desc}\n')
+            f.write(f'" ProjectID: {projectid}\n')
+            f.write('\n')
+            f.write(dedent(contents).rstrip())
+            f.write('\n')
+
+    return _factory
