30b9eb4fa9f33331af9e9ec18df5f1c38586b92f
Peter Hodge <peter.hodge84@gmail.com>
2024-05-03 15:46:28 +1000

    Refactor tests

--- tests/e2e/test_use_case_set_custom_filetype.py
+++ tests/e2e/test_use_case_set_custom_filetype.py
@@ -1,16 +1,25 @@
-def test_project_config_that_sets_custom_filetype(
-    tmpdir,
-    ieditor,
+from textwrap import dedent
+
+import pytest
+
+
+@pytest.fixture()
+def placeholder_repo_configs(
     git_repo_a,
     git_repo_b,
     git_repo_c,
     repo_config_factory,
-):
-    """
-    Confirm that Example 1 from examples.md works as documented.
-    """
+) -> None:
     # create three different project configs that will set filetype by extension
-    code_template = "fun! projectconfig.api2024.filetypeDetect(bufnr, ext, basename, bufname) dict\nif a:ext == 'py'\nreturn '{pyft}'\nendif\nendfun\n"
+    code_template = dedent(
+        """
+        fun! projectconfig.api2024.filetypeDetect(bufnr, ext, basename, bufname) dict
+            if a:ext == 'py'
+                return '{pyft}'
+            endif
+        endfun
+        """.lstrip()
+    )
     cfg1 = code_template.format(cfgname="cfg1", pyft='python1')
     cfg2 = code_template.format(cfgname='cfg2', pyft='python2')
     cfg3 = code_template.format(cfgname='cfg3', pyft='python3')
@@ -19,7 +28,15 @@ def test_project_config_that_sets_custom_filetype(
     repo_config_factory(git_repo_b, 'repo-b', 'Repo B', cfg2)
     repo_config_factory(git_repo_c, 'repo-c', 'Repo C', cfg3)
 
-    pairs = [
+
+@pytest.fixture
+def file_pairs(
+    tmpdir,
+    git_repo_a,
+    git_repo_b,
+    git_repo_c,
+):
+    return [
         (git_repo_a.path, git_repo_a.pyfile, 'python1'),
         (git_repo_b.path, git_repo_b.pyfile, 'python2'),
         (git_repo_c.path, git_repo_c.pyfile, 'python3'),
@@ -27,21 +44,29 @@ def test_project_config_that_sets_custom_filetype(
         (tmpdir, tmpdir / 'some_file.py', 'python'),
     ]
 
-    # assertions
 
-    # Assertion1 - custom filetypes work when opening an initial file for the project
-    for cwd, pyfile, expected_filetype in pairs:
-        ieditor.launch(cwd, pyfile.relative_to(cwd))
-        assert ieditor.get_expr_str('&filetype') == expected_filetype
+@pytest.mark.usefixtures("placeholder_repo_configs")
+class TestProjectConfigWithCustomFileTypes:
+    def test_launching_edit_of_project_file(self, ieditor, file_pairs):
+        # custom filetypes work when opening an initial file for the project
+        for cwd, pyfile, expected_filetype in file_pairs:
+            ieditor.launch(cwd, pyfile.relative_to(cwd))
+            assert ieditor.get_expr_str('&filetype') == expected_filetype
+            ieditor.quitall()
+
+    def test_split_to_project_file_after_launch(
+        self,
+        tmpdir,
+        ieditor,
+        git_repo_a,
+        file_pairs,
+    ):
+        """Custom filetypes work when opening existing file."""
+        # Open each repo's .py file in a single vim instance so that all custom
+        # extensions are registered.
+        ieditor.launch(git_repo_a.path, tmpdir / 'some_python_file.py')
+        assert ieditor.get_expr_str('&filetype') == 'python'  # a regular python file
+        for _, pyfile, expected_filetype in file_pairs:
+            ieditor.edit(pyfile)
+            assert ieditor.get_expr_str('&filetype') == expected_filetype
         ieditor.quitall()
-
-    # Assertion 2 - custom filetypes work when opening existing file
-    #
-    # Open each repo's .py file in a single vim instance so that all custom
-    # extensions are registered.
-    ieditor.launch(git_repo_a.path, tmpdir / 'some_python_file.py')
-    assert ieditor.get_expr_str('&filetype') == 'python'  # a regular python file
-    for _, pyfile, expected_filetype in pairs:
-        ieditor.edit(pyfile)
-        assert ieditor.get_expr_str('&filetype') == expected_filetype
-    ieditor.quitall()
